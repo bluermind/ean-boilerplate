7ca79a55754c16e3ad102b7342ec913aa3f22e95
Jiri Spac <jiri.spac.contractor@avg.com>
2014-03-13 12:36:54 +0100

    bootstrap and bower deps update

--- public/js/routes.js
+++ public/js/routes.js
@@ -3,6 +3,7 @@
     //application routing
     exports.routes = [
         { route:'/', resolve: {templateUrl:'/templates/root.html'}},
+        { route:'/about', resolve: {templateUrl:'/templates/about.html'}},
         { route:'/route', resolve: {templateUrl:'/templates/root.html', reloadOnSearch: false}}
 
     ];
