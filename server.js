2ad4feb4ed2f25b14c03a5c17b3a6ef12d4cf7b4
Jiri Spac <capajj@gmail.com>
2014-04-21 03:32:33 +0200

    expressjs 4.0 integrated, simple sitemap generation integrated

--- server.js
+++ server.js
@@ -3,71 +3,49 @@
  * Module dependencies.
  */
 var logger = require('winston');
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
- */
-var pjson = require('./package.json');
 
-//Load configurations
-//if test env, load example file
-var env = process.env.NODE_ENV = pjson.env || 'dev';
+var pjson = require('./package.json');
+var env = process.env.NODE_ENV = pjson.env || 'development';
 
 var express = require('express');
-var pathChecker = require('./server/pathChecker.js');
+var pathChecker = require('./server/path_checker.js');
 var app = module.exports = express();
-var fs = require('fs');
 
-
-app.configure(function(){
-    app.set('port', process.env.PORT || pjson.port);
-    app.set('showStackError', true);
-
-    //Should be placed before express.static
-    app.use(express.compress({
+if (env === 'production') {
+    app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000/'));
+    app.use(require('compression')({
         filter: function(req, res) {
             return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
         },
         level: 9
     }));
 
-    //Setting the fav icon and static folder
-    app.use(express.favicon());
-    app.use(express.urlencoded());
-    app.use(express.json());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    if (env == 'production') {
-        app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000/'));
-    }
-});
+    require('./server/sitemap_from_routes')(app, 'http://localhost:8080');  //simple sitemap generated from angular routes definition
+}
 
-//
-//process.on('uncaughtException', function(err) {
-//    console.log(err);
-//});
+app.use(require('static-favicon')());
+app.set('showStackError', true);
 
-var server = app.listen(app.get('port'), function () {
+app.use(express.static(__dirname + '/public'));
+app.use(require('morgan')('dev'));
+
+app.use(require('body-parser')()); 						// pull information from html in POST
+app.use(require('method-override')()); 					// simulate DELETE and PUT
+
+var port = process.env.PORT || pjson.port;
+var server = app.listen(port, function () {
 
     app.get('*', function(req, res){
         var pathName = req._parsedUrl.pathname;
-        var filePath = './public' + pathName;
-        fs.exists(filePath, function (exists)
-        {
-            if(exists)
-            {
-                res.sendfile(filePath);
-            } else {
-                if(pathChecker(pathName)){
-                    res.sendfile('./public/index.html');
-                } else {
-                    res.status(404);
-                    res.sendfile('./public/index.html');
-                }
-            }
 
-        });
+        if(pathChecker(pathName)){
+            res.sendfile('./public/index.html');
+        } else {
+            res.status(404);
+            res.sendfile('./public/index.html');
+        }
 
     });
-    console.info("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    logger.info("Express server listening on port %d in %s mode", this.address().port, env);
 });
+
--- server/config.js
+++
@@ -1,7 +0,0 @@
-var _ = require('lodash');
-
-// Load app configuration
-
-module.exports = _.extend(
-    require(__dirname + '/../config/all.js'),
-    require(__dirname + '/../config/' + process.env.NODE_ENV + '.json') || {});
